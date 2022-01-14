2367919f239c7d3b25fdec821f50834577f106a0
Gon Kim <87866588+Gonnnnn@users.noreply.github.com>
2022-01-14 13:47:03 +0000

    Baekjoon Greedy 1080 / For some reason, a for loop in another for loop is not executed as many times as it's supposed to be

---
+++ Baekjoon/Greedy/1080_행렬.py
@@ -0,0 +1,66 @@
+# 문제
+# 0과 1로만 이루어진 행렬 A와 행렬 B가 있다. 이때, 행렬 A를 행렬 B로 바꾸는데 필요한 연산의 횟수의 최솟값을 구하는 프로그램을 작성하시오.
+
+# 행렬을 변환하는 연산은 어떤 3×3크기의 부분 행렬에 있는 모든 원소를 뒤집는 것이다. (0 → 1, 1 → 0)
+
+# 입력
+# 첫째 줄에 행렬의 크기 N M이 주어진다. N과 M은 50보다 작거나 같은 자연수이다. 둘째 줄부터 N개의 줄에는 행렬 A가 주어지고, 그 다음줄부터 N개의 줄에는 행렬 B가 주어진다.
+
+# 출력
+# 첫째 줄에 문제의 정답을 출력한다. 만약 A를 B로 바꿀 수 없다면 -1을 출력한다.
+
+import sys
+
+
+n, m = map(int, input().split())
+
+a = []
+for _ in range(n):
+  a.append(list(map(int, input().strip())))
+
+b = []
+for _ in range(n):
+  b.append(list(map(int, input().strip())))
+
+
+if((n < 3) or (m < 3)):
+  print(-1)
+else:
+  count = 0
+  possible = True
+  for j in range(1, n - 1):
+    
+    if(not possible):
+      break
+
+    for i in range(1, m - 1):
+      if(i == m - 2):
+        for k in range(2):
+          if((a[j-1][i-1+k] == b[j-1][i-1+k]) ^ (a[j-1][i-1+k] == b[j-1][i-1+k])):
+            possible = False
+            
+      if(a[j-1][i-1] != b[j-1][i-1]):
+        count += 1
+        for l in range(3):
+          for m in range(3):
+            if(a[j-1+l][i-1+m] == 1):
+              a[j-1+l][i-1+m] = 0
+            else:
+              a[j-1+l][i-1+m] = 1
+
+
+  if(not possible):
+    print(-1)
+  else:
+    no_answer = False
+    for j in range(2):
+      if(no_answer):
+        break
+      for i in range(m-1):
+        if(a[j][i] != b[j][i]):
+          no_answer == True
+          break
+    if(no_answer):
+      print(-1)
+    else:
+      print(count)
