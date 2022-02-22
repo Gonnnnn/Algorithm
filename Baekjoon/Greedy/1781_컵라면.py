9e14d3315b9f582ee2d6d5d695d9b43922b8051a
Gon Kim <87866588+Gonnnnn@users.noreply.github.com>
2022-02-22 07:37:37 +0000

    Baekjoon Greedy 1781

---
+++ Baekjoon/Greedy/1781_컵라면.py
@@ -0,0 +1,21 @@
+import sys
+import heapq as hq
+
+input = sys.stdin.readline
+
+N = int(input())
+# [deadline, num of cup ramen]
+table = [list(map(int, input().split())) for _ in range(N)]
+table.sort()
+
+result = []
+for i in range(N):
+  d, ramen = table[i]
+  if len(result) < d:
+    hq.heappush(result, ramen)
+  else:
+    if result[0] < ramen:
+      hq.heappop(result)
+      hq.heappush(result, ramen)
+
+print(sum(result))
