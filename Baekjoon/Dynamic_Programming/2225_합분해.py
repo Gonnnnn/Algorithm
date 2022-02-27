450dc4fc361fad0e212c8ad13848ca8534679ce0
Gon Kim <87866588+Gonnnnn@users.noreply.github.com>
2022-02-27 06:53:20 +0000

    Baekjoon DP 2225

--- Baekjoon/Dynamic_Programming/2225_합분해.py
+++ Baekjoon/Dynamic_Programming/2225_합분해.py
@@ -1,1 +1,10 @@
 N, K = map(int, input().split())
+DP = [[0] * (N+1) for _ in range(K)]
+for i in range(N+1):
+  DP[0][i] = 1
+
+for i in range(1, K):
+  for j in range(N+1):
+    DP[i][j] = sum(DP[i-1][:j+1])%1000000000
+
+print(DP[-1][-1])
