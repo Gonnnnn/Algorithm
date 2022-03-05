f7fd57667a748ed20811b467a1e54fbca125c712
Gon Kim <87866588+Gonnnnn@users.noreply.github.com>
2022-03-05 14:18:10 +0000

    Baekjoon DP 2565

---
+++ Baekjoon/Dynamic_Programming/2565_전깃줄.py
@@ -0,0 +1,16 @@
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+table = [list(map(int, input().split())) for _ in range(N)]
+table.sort()
+DP = [0]*N
+for i in range(N):
+  temp = 0
+  for j in range(0, i):
+    if table[j][1] < table[i][1]:
+      if temp < DP[j]:
+        temp = DP[j]
+  DP[i] = 1 + temp
+
+print(N-max(DP))
