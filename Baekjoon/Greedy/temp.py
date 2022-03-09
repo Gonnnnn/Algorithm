f4d4efa09cea0c8c209873c13748df48e7f84946
Gon Kim <87866588+Gonnnnn@users.noreply.github.com>
2022-03-09 04:47:46 +0000

    Baekjoon Greedy 13413

--- Baekjoon/Greedy/temp.py
+++ Baekjoon/Greedy/temp.py
@@ -1,15 +1,5 @@
-N, M = map(int, input().split())
-req = [list(map(int, input().split())) for _ in range(M)]
-req.sort(key=lambda x:x[1])
+# print(bin(10))
+# print(bin(10)[::-1].index('1'))
 
-cnt = 0
-visited = [False]*(N+1)
-for a, b in req:
-  for i in range(a, b+1):
-    if not visited[i]:
-      visited[i] = True
-      cnt += 1
-      print(visited)
-      break
-
-print(cnt)
+a = '000'
+print(list(a))
