0b0523a8dcc8225840bcc34de22f15c218bf0954
Gon Kim <87866588+Gonnnnn@users.noreply.github.com>
2022-09-25 20:28:14 +0900

    [Gold V] Title: 탑, Time: 360 ms, Memory: 231028 KB -BaekjoonHub

--- 백준/Gold/2493. 탑/탑.py
+++ 백준/Gold/2493. 탑/탑.py
@@ -3,20 +3,14 @@ towers = list(map(int, input().split(' ')))
 stk = []
 result = []
 for i, tower in enumerate(towers):
-    if not stk:
-        stk.append([i, tower])
-        result.append('0')
-        continue
-    if stk[-1][1] > tower:
-        result.append(stk[-1][0]+1)
-        stk.append([i, tower])
-    else:
-        while(stk and stk[-1][1] < tower):
-            stk.pop()
-        if not stk:
-            result.append('0')
+    while stk:
+        if stk[-1][1] > tower:            
+            break
         else:
-            result.append(stk[-1][0]+1)
-        stk.append([i, tower])
-
+            stk.pop()
+    if stk:
+        result.append(stk[-1][0]+1)
+    else:
+        result.append('0')
+    stk.append([i, tower])
 print(' '.join(map(str, result)))
