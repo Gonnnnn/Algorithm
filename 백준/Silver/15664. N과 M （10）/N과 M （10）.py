20ab5f7ed4aa7157fcc8ea7f65bd4d0be4d775b2
Gon Kim <87866588+Gonnnnn@users.noreply.github.com>
2022-10-06 14:30:07 +0900

    [Silver II] Title: N과 M (10), Time: 108 ms, Memory: 113248 KB -BaekjoonHub

---
+++ 백준/Silver/15664. N과 M （10）/N과 M （10）.py
@@ -0,0 +1,21 @@
+import sys
+input = sys.stdin.readline
+
+n, m = map(int, input().strip().split(' '))
+randNums = list(map(int, input().strip().split(' ')))
+randNums.sort()
+
+def solve(nums, start):
+    global n
+    if len(nums) == m:
+        print(' '.join(list(map(str, nums))))
+        return
+    temp = 0
+    for i in range(start, n):
+        if temp == randNums[i]: continue
+        temp = randNums[i]
+        nums.append(randNums[i])
+        solve(nums, i + 1)
+        nums.pop()
+    return
+solve([], 0)
