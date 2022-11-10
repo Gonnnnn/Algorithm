798c8b3e92936d40419aee0be91eac345bf346fd
Gon Kim <87866588+Gonnnnn@users.noreply.github.com>
2022-11-10 10:40:27 +0900

    [Gold V] Title: 수 고르기, Time: 164 ms, Memory: 116496 KB -BaekjoonHub

--- 백준/Gold/2230. 수 고르기/수 고르기.py
+++ 백준/Gold/2230. 수 고르기/수 고르기.py
@@ -5,19 +5,11 @@ n, m = map(int, input().strip().split())
 nums = [int(input().strip()) for _ in range(n)]
 nums.sort()
 
-def binarySearch(st, target):
-    en = len(nums)
-    while(st < en):
-        mid = (st + en) // 2
-        if nums[mid] < target: st = mid + 1
-        else: en = mid
-    return st
-
+st, en = 0, 0
 answer = float('inf')
-for i, num in enumerate(nums):
-    target = m + num
-    idx = binarySearch(i, target)
-    if(idx == len(nums)): continue
-    answer = min(answer, nums[idx] - num)
-
+while(en < n and st < n):
+    if(nums[en] - nums[st] < m): en += 1
+    else:
+        answer = min(answer, nums[en] - nums[st])
+        st += 1
 print(answer)
