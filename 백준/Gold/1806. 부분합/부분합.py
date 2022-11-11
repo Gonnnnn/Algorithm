01d56a2abe2b93c8cac31115308d60f716c5c156
Gon Kim <87866588+Gonnnnn@users.noreply.github.com>
2022-11-11 10:22:43 +0900

    [Gold IV] Title: 부분합, Time: 140 ms, Memory: 128628 KB -BaekjoonHub

--- 백준/Gold/1806. 부분합/부분합.py
+++ 백준/Gold/1806. 부분합/부분합.py
@@ -8,15 +8,15 @@ total = nums[en]
 gap = n + 1
 
 while(st < n and en < n):
+    if st > en:
+        en = st
+        total += nums[en]
+        continue
     if total >= s:
         gap = min(gap, en - st + 1)
         total -= nums[st]
         st += 1
-        if st > en and st < n:
-            en = st
-            total += nums[en]
     else:
         en += 1
-        if en < n:
-            total += nums[en]
+        if en < n: total += nums[en]
 print(gap if gap < n + 1 else 0)
