5461aa64cc39cfa3b8b09ec25e9ae081d5d56918
Gon Kim <87866588+Gonnnnn@users.noreply.github.com>
2022-11-26 01:52:07 +0900

    [level 3] Title: 입국심사, Time: 0.03 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv3/43238. 입국심사/입국심사.py
+++ 프로그래머스/lv3/43238. 입국심사/입국심사.py
@@ -6,28 +6,6 @@ def solution(n, times):
         numOfPpl = 0
         for time in times:
             numOfPpl += mid // time
-        # possible = canFinishInTime(mid, n, times)
         if numOfPpl >= n: r = mid
         else: l = mid + 1    
-
     return l
-
-def canFinishInTime(targetTime, n, times):
-    가능한_심사관_수 = 0
-    for time in times:
-        if time <= targetTime:
-            가능한_심사관_수 += 1
-        else:
-            break
-
-    커버할수있는_모든_사람수 = 0
-    for i in range(가능한_심사관_수):
-        time = times[i]
-        심사관이_커버할수있는_사람수 = targetTime // time
-        커버할수있는_모든_사람수 += 심사관이_커버할수있는_사람수
-    
-    if 커버할수있는_모든_사람수 < n:
-        return False
-    
-    return True
-        
