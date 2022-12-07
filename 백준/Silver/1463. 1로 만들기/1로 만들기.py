eef4f0dc8abdc74fa8412d42188df5307e754c35
Gon Kim <87866588+Gonnnnn@users.noreply.github.com>
2022-12-07 22:11:04 +0900

    [Silver III] Title: 1로 만들기, Time: 144 ms, Memory: 122304 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.py
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.py
@@ -3,21 +3,12 @@ import sys
 input = sys.stdin.readline
 
 N = int(input())
-if N == 1:
-    print(0)
-elif N == 2 or N == 3:
-    print(1)
-else:
-    dp = [0 for _ in range(N + 1)]
-    dp[1] = 0
-    dp[2] = 1
-    dp[3] = 1
-    for i in range(4, N + 1):
-        temp = float('inf')
-        if i % 3 == 0:
-            temp = min(temp, dp[i // 3])
-        if i % 2 == 0:
-            temp = min(temp, dp[i // 2])
-        temp = min(temp, dp[i - 1])
-        dp[i] = temp + 1
-    print(dp[N])
+dp = [0 for _ in range(N + 1)]
+for i in range(2, N + 1):
+    temp = dp[i - 1]
+    if i % 3 == 0:
+        temp = min(temp, dp[i // 3])
+    if i % 2 == 0:
+        temp = min(temp, dp[i // 2])
+    dp[i] = temp + 1
+print(dp[N])
