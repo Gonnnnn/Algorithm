9f7e8b7db3ae1a3c89e7cbc0e0a62b1cdf4cd81a
Gon Kim <87866588+Gonnnnn@users.noreply.github.com>
2022-12-28 17:17:59 +0900

    [Silver I] Title: 달력, Time: 132 ms, Memory: 114328 KB -BaekjoonHub

--- 백준/Silver/20207. 달력/달력.py
+++ 백준/Silver/20207. 달력/달력.py
@@ -1,25 +1,13 @@
 import sys
-import heapq
 
 input = sys.stdin.readline
 N = int(input())
-pq = [(lambda x: (x[0], -1 * (x[1] - x[0] + 1)))(list(map(int, input().split(' ')))) for _ in range(N)]
-remains = []
-heapq.heapify(pq)
-
 height = [0 for _ in range(365 + 1)]
-while(pq):
-    row = [False for _ in range(365 + 1)]
-    for _ in range(len(pq)):
-        day, length = heapq.heappop(pq)
-        if row[day]:
-            remains.append((day, length))
-            continue
-        for i in range(length * -1):
-            row[day + i] = True
-            height[day + i] += 1
-    pq = remains
-    remains = []
+for _ in range(N):
+    S, E = map(int, input().split(' '))
+    for i in range(S, E + 1):
+        height[i] += 1
+        
 answer = 0
 Max = 0
 length = 0
