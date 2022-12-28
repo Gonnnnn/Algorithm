0236714adb7e770dbf6939a79b69225719255c3c
Gon Kim <87866588+Gonnnnn@users.noreply.github.com>
2022-12-29 06:35:17 +0900

    [Silver I] Title: 외계인의 기타 연주, Time: 396 ms, Memory: 156688 KB -BaekjoonHub

--- 백준/Silver/2841. 외계인의 기타 연주/외계인의 기타 연주.py
+++ 백준/Silver/2841. 외계인의 기타 연주/외계인의 기타 연주.py
@@ -4,16 +4,15 @@ import heapq
 input = sys.stdin.readline
 # 음의 개수, 프렛 수
 N, P = map(int, input().split(' '))
-pqs = [[] for _ in range(N + 1)]
+pressed = [[0] for _ in range(N + 1)]
 answer = 0
 for _ in range(N):
     stringNum, fret = map(int, input().split(' '))
-    while(pqs[stringNum]):
-        peek = pqs[stringNum][0] * -1
-        if peek <= fret: break
-        heapq.heappop(pqs[stringNum])
+    # 최악의 경우 해당 줄에서 눌린 fret을 다 떼어내야한다고 하더라도, 이를 모두 눌러가는데 그만큼의 상수 시간이 걸린다.
+    while(fret < pressed[stringNum][-1]):
+        pressed[stringNum].pop()
         answer += 1
-    if pqs[stringNum] and pqs[stringNum][0] * -1 == fret: continue
-    heapq.heappush(pqs[stringNum], fret * -1)
+    if pressed[stringNum][-1] == fret: continue
+    pressed[stringNum].append(fret)
     answer += 1
 print(answer)
