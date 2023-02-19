661662f8d21b4fe9255a623ee57677ab7672bbc7
Gon Kim <87866588+Gonnnnn@users.noreply.github.com>
2023-02-19 23:36:19 +0900

    [Silver V] Title: 단어 정렬, Time: 204 ms, Memory: 119560 KB -BaekjoonHub

---
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.py
@@ -0,0 +1,6 @@
+import sys
+
+input = sys.stdin.readline
+N = int(input())
+words = sorted(list(set([input().strip() for _ in range(N)])), key = lambda x:(len(x), x))
+for word in words: print(word)
